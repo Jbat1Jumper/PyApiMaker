f47a812d098e235672e767c8eaa180967dbee021
Nikita Zdanovitch <3hb.tch@gmail.com>
2014-05-03 06:10:38 -0300

    limpieza y al main

--- test/js_interactive/web_server.py
+++ test/js_interactive/web_server.py
@@ -13,16 +13,13 @@ def root(res):
         ".html": "text/html",
         ".js": "application/javascript",
     }
-    with ZipFile("./resources.zip", "r") as z:
+    with ZipFile("./resources", "r") as z:
         with z.open(res, "r") as f:
             f = io.TextIOWrapper(f)
             m = mimetypes[(os.path.splitext(res)[1])]
             resp = Response(f.read(), mimetype=m)
-            resp.headers["Access-Control-Allow-Origin"] = "localhost, "\
-                                                          "localhost:16319"
-            resp.headers["Access-Control-Allow-Methods"] = "POST, GET"
             return resp
 
 
 if __name__ == "__main__":
-    app.run(port=80, debug=True)
+    app.run(port=80)
