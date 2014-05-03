682e331587d4b7b9d5b35955f369aff2568cf2b2
Nikita Zdanovitch <3hb.tch@gmail.com>
2014-05-03 00:59:13 -0300

    updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='PyApiMaker',
-    version='0.1.0',
+    version='0.1.1',
     author='Nikita Zdanovitch',
     author_email='nzdanovitch@dc.uba.ar',
     packages=['pyapimaker'],
