e31e9237ffe036a6c5c608640bae38c8da78d09a
Nikita Zdanovitch <3hb.tch@gmail.com>
2014-05-02 23:21:56 -0300

    edited setup.py

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ setup(
     author='Nikita Zdanovitch',
     author_email='nzdanovitch@dc.uba.ar',
     packages=['pyapimaker'],
-    url=None,
+    url='https://github.com/Jbat1Jumper/PyApiMaker.git',
     license='LICENSE.txt',
     description='Provides utilities to manage and interact with your code.',
-    long_description=open('README.rst').read(),
+    long_description=open('README.rst', 'r').read(),
     install_requires=["flask"
     ],
 )
