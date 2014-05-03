b2d1ea55cb625681f21196f52ed71bc03c6cfefe
Nikita Zdanovitch <3hb.tch@gmail.com>
2014-05-03 00:49:13 -0300

    hello again

---
+++ CHANGES.rst
@@ -0,0 +1,23 @@
+=========
+Changelog
+=========
+
+Just a changelog template, for now
+
+0.1.0 (2014-05-1)
+=================
+
+New
+---
+
+- Initial release
+
+Changes
+-------
+
+- Empty
+
+Fix
+---
+
+- Empty
---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='PyApiMaker',
+    version='0.1.0',
+    author='Nikita Zdanovitch',
+    author_email='nzdanovitch@dc.uba.ar',
+    packages=['pyapimaker'],
+    url='https://github.com/Jbat1Jumper/PyApiMaker.git',
+    license='LICENSE.txt',
+    description='Provides utilities to manage and interact with your code.',
+    long_description="Some long description must go here",
+    install_requires=["flask"
+    ]
+)
