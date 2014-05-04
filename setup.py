53221876ddb949f97d19c9b1eb4491787deb9687
Nikita Zdanovitch <3hb.tch@gmail.com>
2014-05-04 05:24:14 -0300

    increased version and pushed to master

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='PyApiMaker',
-    version='0.1.3',
+    version='0.1.4',
     author='Nikita Zdanovitch',
     author_email='nzdanovitch@dc.uba.ar',
     packages=['pyapimaker'],
